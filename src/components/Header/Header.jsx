02b26ab23dd417e9c8601f63bf9d1839521833c7
SandeepJoshi <joshisandip111@gmail.com>
2023-05-19 22:37:49 +0545

    Commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,20 @@ import { useEffect, useState } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/firestore";
 import "firebase/compat/auth";
-import './header.css'
-import HOMEDOC from '../../assets/home doc.png'
+import "./header.css";
+import HOMEDOC from "../../assets/undraw_doctors_p6aq.svg";
 
 function Header() {
-  const [clipPath, setClipPath] = useState('initial');
+  const [clipPath, setClipPath] = useState("initial");
 
   return (
-    <div  className="container header-container">
-        <div className="header-content" style={{ clipPath }}>
+    <div className="container header-container">
+      <div className="header-content" style={{ clipPath }}>
+        <div className="home-image">
           <img src={HOMEDOC} alt="" />
-      
         </div>
-  </div>
+      </div>
+    </div>
   );
 }
 
