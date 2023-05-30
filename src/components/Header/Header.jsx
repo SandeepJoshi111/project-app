8c677ad5fa60ad0cc4a1ca4f7dfd09c2248f3ee7
SandeepJoshi <joshisandip111@gmail.com>
2023-05-30 22:39:29 +0545

    changed heartbeat svg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -145,19 +145,14 @@ function Header() {
           <h1 className="home-title">Contact Us</h1>
           {/* <div className="contact-divider"> */}
           <div className="heatbeat-content">
-            <svg
-              className="heartbeat"
-              width="800"
-              height="600"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g class="pathdraw" id="Layer_1">
-                <title>Pulse Heart</title>
-                <path
-                  id="svg_1"
-                  d="m162.5,299.2l142.5,-0.2l8,-23l11,23l34,0l14,-109l14,226l12,-118l30,0l5,-15l10,0l7,-16l10,31l155,0"
-                />
-              </g>
+            <svg className="heartbeat" x="0px" y="0px" viewBox="0 0 298 53.9">
+              <path
+                class="pathdraw"
+                d="M297.5,41.2h-76.6c-0.5,0-0.9,0.4-1,0.8l-1.6,11.3l-3.1-32c0-0.5-0.4-0.9-0.9-0.9c-0.5,0-0.9,0.3-1,0.8
+                       l-5.3,25.5l-2.3-10.9c-0.1-0.4-0.4-0.7-0.9-0.8c-0.4,0-0.8,0.2-1,0.6l-2.3,4.8h-107c0,0,0,0,0,0H82c-1.6,0-2.2,1.1-2.2,1.6
+                       l-1.6,11.3l-3.1-52c0-0.5-0.4-0.9-0.9-0.9c-0.5,0-0.9,0.3-1,0.8l-9.3,45.5l-2.3-10.9c-0.1-0.4-0.4-0.7-0.9-0.8c-0.4,0-0.8,0.2-1,0.6
+                       l-2.3,4.8H0.5"
+              />
             </svg>
           </div>
           <div className="contact-content">
